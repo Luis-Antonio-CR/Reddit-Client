d866dcef77d26f36943e1cdc019048b85ee582df
Luis-Antonio-CR <96863185+Luis-Antonio-CR@users.noreply.github.com>
2022-06-03 19:06:26 +0200

    Style the main page, add the gitignore for the node_modules folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
- const App = () => {
+import './style/App.css';
+import NavBar from "./components/NavBar";
+//import PostComp from "./components/PostComponent";
+import CategoryBar from "./components/CategoriesBar";
+import SubredditsBar from "./components/SubredditsBar";
+
+const App = () => {
     return(
-      <div>
-        <h1>Hello world</h1>
+      <div className='body'>
+        <h1>Hello</h1>
+        <NavBar />
+        <div className='main'>
+          <section className='posts'>
+
+          </section>
+          <section className='lateralBar'>
+            <CategoryBar />
+            <SubredditsBar />
+          </section>
+        </div>
       </div>
     )
- }
+}
 
  export default App;
