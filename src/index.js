5b5c820fad5b79493ecd22830748706bdbe6ba30
Luis-Antonio-CR <96863185+Luis-Antonio-CR@users.noreply.github.com>
2022-06-03 17:02:13 +0200

    Creation of empty components and initial app stucture

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDom from "react-dom";
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import App from "./App"
+
+ReactDom.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
-);
+  </BrowserRouter>, 
+  document.getElementById('root')
+)
