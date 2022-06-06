2c71f1a5e6763e74e377ac042806f38fd4a93bd1
Luis-Antonio-CR <96863185+Luis-Antonio-CR@users.noreply.github.com>
2022-06-06 19:07:48 +0200

    Creation the style of the post component

--- src/components/PostComponent.js
+++ src/components/PostComponent.js
@@ -1,9 +1,23 @@
+import imgPost from "../assets/imgPost.jpeg";
+import arrow from "../assets/Arrow.png";
+
 const PostComp = () => {
     return(
         <div className="postComp">
-            <div className="postComp-lateral"></div>
-            <div className="postComp-main"></div>
-            <div className="postComp-baseline"></div>
+            <div className="postComp-lateral">
+                <img src={arrow} className="arrow_top"/>
+                <h3 className="lateral_number">Number</h3>
+                <img src={arrow} className="arrow_bottom"/>
+            </div>
+            <div className="postComp-main">
+                <h2 className="medium_text">Title</h2>
+                <img src={imgPost}/>
+            </div>
+            <div className="postComp-baseline">
+                <h3>Comments</h3>
+                <h3>Time</h3>
+                <h3>Author</h3>
+            </div>
         </div>
     )
 }
