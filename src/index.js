17d9d4db9a640673aea09df31ee7cd2d493d4e8c
Luis-Antonio-CR <96863185+Luis-Antonio-CR@users.noreply.github.com>
2022-08-16 15:22:58 +0200

    Adding Api file
    in the api file we have the fetch for subreddits posts, subreddits and post comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
 
 import App from "./App"
 
 ReactDom.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, 
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  , 
   document.getElementById('root')
 )
